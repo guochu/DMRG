c870ab3d573a10e6d965c2f921dd7f8cf59e4ba9
guo chu <guochu604b@gmail.com>
2024-01-17 08:54:15 +0800

    minor fix

--- src/states/bondview.jl
+++ src/states/bondview.jl
@@ -7,7 +7,7 @@ end
 Base.getindex(psi::MPSBondView, i::Int) = getindex(psi.parent.svectors, i)
 Base.firstindex(m::MPSBondView) = firstindex(m.parent.svectors)
 Base.lastindex(m::MPSBondView) = lastindex(m.parent.svectors)
-function Base.setindex!(m::MPSBondView, v::MPSBondTensor, i::Int)
+function Base.setindex!(m::MPSBondView, v, i::Int)
 	# check_bondtensor_dir(v) || throw(SpaceMismatch())
 	L = length(m.parent)
 	(1 < i <= L) || throw(BoundsError(m.parent.svectors, i))
